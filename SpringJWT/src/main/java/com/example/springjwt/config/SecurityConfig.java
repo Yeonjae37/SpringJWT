fb03d4fe96e8be3e140f03163753c7612c3a9608
Yeonjae37 <jyj030307@gmail.com>
2024-06-30 19:44:03 +0900

    feat: UserEntity 생

--- SpringJWT/src/main/java/com/example/springjwt/config/SecurityConfig.java
+++ SpringJWT/src/main/java/com/example/springjwt/config/SecurityConfig.java
@@ -16,6 +16,7 @@ public class SecurityConfig {
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
+    //비밀번호를 해시화하여 저장함. 반복적인 해시를 적용하여 브루트포스 공격에 대한 저항력을 높임
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -36,13 +37,17 @@ public class SecurityConfig {
         http
                 .authorizeHttpRequests((auth) -> auth
                         .requestMatchers("/login", "/", "join").permitAll()
+                        //인증되지 않은 사용자도 위 경로에 접근 가능
                         .requestMatchers("/admin").hasRole("ADMIN")
+                        // /admin 경로에 대한 요청은 ADMIN 역할을 가진 사용자만 접근 가능
                         .anyRequest().authenticated());
+                        //나머지 모든 요청은 인증된 사용자만 접근 가능 (로그인 된 사용자)
 
         //세션 설정
         http
                 .sessionManagement((session) -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+                        // 인증 처리적 관점에서 세션을 생성하지 않음과 동시에 세션을 이용한 방식으로 인증을 처리하지 않겠다는 의미
 
         return http.build();
     }
--- SpringJWT/src/main/java/com/example/springjwt/controller/AdminController.java
+++ SpringJWT/src/main/java/com/example/springjwt/controller/AdminController.java
@@ -10,6 +10,6 @@ public class AdminController {
 
     @GetMapping("/admin")
     public String adminP(){
-        return "admin Controller"
+        return "admin Controller";
     }
 }
